e4d4ce80b7f3b62b2975cbf9a164b5eaf72670ed
Reverie Wisp <contact@reveriewisp.com>
2019-04-14 21:02:03 -0700

    = Registered command and whatnot

--- src/core/ObjectBuilderFactory.java
+++ src/core/ObjectBuilderFactory.java
@@ -351,9 +351,10 @@ public class ObjectBuilderFactory
 		manager.Register(LocCommands.Stub("tony, stark, dontfeelgood, dontfeelsogood"), new CommandStark(KittyRole.General, KittyRating.Safe));
 		manager.Register(LocCommands.Stub("blur"), new CommandBlurry(KittyRole.General, KittyRating.Safe));
 		manager.Register(LocCommands.Stub("eightball, 8ball"), new CommandEightBall(KittyRole.General, KittyRating.Safe));
+		manager.Register(LocCommands.Stub("defaultdance"), new CommandEightBall(KittyRole.General, KittyRating.Safe));
 		
-		manager.Register("derp", new CommandDerpi(KittyRole.General, KittyRating.Filtered));
-		manager.Register("e621", new CommandE621(KittyRole.General, KittyRating.Filtered));
+		manager.Register(LocCommands.Stub("derp"), new CommandDerpi(KittyRole.General, KittyRating.Filtered));
+		manager.Register(LocCommands.Stub("e621"), new CommandE621(KittyRole.General, KittyRating.Filtered));
 		
 		return manager;
 	}
